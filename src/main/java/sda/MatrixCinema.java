35c9b06dc64ac0082bc9b15b48fa5c1bb1bdce01
Mircea <bodea.mircea@gmail.com>
2019-07-25 23:46:44 +0300

    changed some minor things, buyt I will get the clone from roxana

--- src/main/java/sda/MatrixCinema.java
+++ src/main/java/sda/MatrixCinema.java
@@ -4,13 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinalApplication {
+public class Matrix_Cinema {
 
     //TODO Change the name of the project/this class from FinalApplication to the name of your own
     // project.
     // This should be changed in the POM file as well. See line 12.
     public static void main(String[] args) {
-        SpringApplication.run(FinalApplication.class, args);
+        SpringApplication.run(Matrix_Cinema.class, args);
     }
 
 }
